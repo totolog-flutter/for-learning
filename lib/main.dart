ff3ff49fae4dd142ae7829cca4fd8970cac88348
toru aoki <totolog1126@gmail.com>
2022-10-24 22:42:03 +0900

    add: step4

--- lib/main.dart
+++ lib/main.dart
@@ -45,6 +45,18 @@ class MyApp extends StatelessWidget {
         _buildButtonColumn(color, Icons.share, 'SHARE'),
       ],
     );
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
     return MaterialApp(
         title: 'Flutter layout demo',
         theme: ThemeData(
@@ -55,7 +67,7 @@ class MyApp extends StatelessWidget {
               title: const Text('Flutter layout demo'),
             ),
             body: Column(
-              children: [titleSection, buttonSection],
+              children: [titleSection, buttonSection, textSection],
             )));
   }
 
