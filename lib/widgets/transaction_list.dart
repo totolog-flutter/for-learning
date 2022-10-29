73e3e5cc52e6321c869a6f8b0635cfcb839e978b
TORU AOKI <Toru.Aoki@ibm.com>
2022-10-29 22:35:45 +0900

    add: 108

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -18,48 +18,79 @@ class TransactionList extends StatelessWidget {
           height: 200,
           child: Image.asset('assets/images/waiting.png', fit: BoxFit.cover,))
       ],) : ListView.builder(
-        itemBuilder: (ctx, index) {
+        itemBuilder:(ctx, index) {
           return Card(
-            child: Row(
-              children: [
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Theme.of(context).primaryColor, width: 2)
-                  ),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    '¥: ${transactions[index].amount.toStringAsFixed(0)}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Theme.of(context).primaryColor
-                    ),
+            elevation: 5,
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 30,
+                child: Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: FittedBox(
+                    child: Text('¥${transactions[index].amount}')
                   ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      transactions[index].title,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
-                    ),
-                    Text(
-                      DateFormat('yyyy/MM/dd').format(transactions[index].date),
-                      style: TextStyle(
-                        color: Colors.grey
-                      ),
-                    ),
-                  ],
-                )
-              ],
+              ),
+              title: Text(
+                transactions[index].title,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              subtitle: Text(
+                DateFormat('yyyy/MM/dd').format(transactions[index].date),
+                style: TextStyle(
+                  color: Colors.grey
+                ),
+              ),
             ),
           );
         },
+        // itemBuilder: (ctx, index) {
+        //   return Card(
+        //     child: Row(
+        //       children: [
+        //         Container(
+        //           margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+        //           decoration: BoxDecoration(
+        //             border: Border.all(color: Theme.of(context).primaryColor, width: 2)
+        //           ),
+        //           padding: EdgeInsets.all(10),
+        //           child: Text(
+        //             '¥: ${transactions[index].amount.toStringAsFixed(0)}',
+        //             style: TextStyle(
+        //               fontWeight: FontWeight.bold,
+        //               fontSize: 20,
+        //               color: Theme.of(context).primaryColor
+        //             ),
+        //           ),
+        //         ),
+        //         Column(
+        //           crossAxisAlignment: CrossAxisAlignment.start,
+        //           children: [
+        //             Text(
+        //               transactions[index].title,
+        //               style: TextStyle(
+        //                 color: Colors.black,
+        //                 fontWeight: FontWeight.bold,
+        //                 fontSize: 16,
+        //               ),
+        //             ),
+        //             Text(
+        //               DateFormat('yyyy/MM/dd').format(transactions[index].date),
+        //               style: TextStyle(
+        //                 color: Colors.grey
+        //               ),
+        //             ),
+        //           ],
+        //         )
+        //       ],
+        //     ),
+        //   );
+        // },
         itemCount: transactions.length,
       ),
     );
