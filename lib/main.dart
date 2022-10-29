42600908643928b9b4c0039af1407ec98bd5150a
TORU AOKI <Toru.Aoki@ibm.com>
2022-10-29 11:41:24 +0900

    add: 79

--- lib/main.dart
+++ lib/main.dart
@@ -57,11 +57,16 @@ class MyHomePage extends StatelessWidget {
                     Container(
                       margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                       decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 2)
+                        border: Border.all(color: Colors.purple, width: 2)
                       ),
                       padding: EdgeInsets.all(10),
                       child: Text(
                         tx.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple
+                        ),
                       ),
                     ),
                     Column(
