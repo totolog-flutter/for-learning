95eff518a5b53b6b7d1e8f42fa7690f39786edeb
TORU AOKI <Toru.Aoki@ibm.com>
2022-10-29 15:42:28 +0900

    add: 91

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,51 +11,50 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tx) {
-            return Card(
-              child: Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple, width: 2)
-                    ),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '¥: ${tx.amount.toString()}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple
-                      ),
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple, width: 2)
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '¥: ${transactions[index].amount.toString()}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple
                     ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      transactions[index].title,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
                       ),
-                      Text(
-                        DateFormat('yyyy/MM/dd').format(tx.date),
-                        style: TextStyle(
-                          color: Colors.grey
-                        ),
+                    ),
+                    Text(
+                      DateFormat('yyyy/MM/dd').format(transactions[index].date),
+                      style: TextStyle(
+                        color: Colors.grey
                       ),
-                    ],
-                  )
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
       ),
     );
   }
