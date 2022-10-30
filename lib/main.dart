3d75ea99939cafe8258c693e8ef13bf2d38dbc30
TORU AOKI <Toru.Aoki@ibm.com>
2022-10-30 09:33:25 +0900

    add: 123

--- lib/main.dart
+++ lib/main.dart
@@ -102,7 +102,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ) * 0.4,
               child: Chart(_recentTransactions)),
             Container(
-              height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.6,
+              height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.7,
               child: TransactionList(transactions: _userTransactions, deleteTransaction: _deleteTransaction)),
           ],
         ),
