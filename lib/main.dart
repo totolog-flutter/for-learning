41ac00dc0525c32a0302e19ca869b4b4799ff0d3
TORU AOKI <Toru.Aoki@ibm.com>
2022-10-31 16:15:13 +0900

    add: 172

--- lib/main.dart
+++ lib/main.dart
@@ -14,33 +14,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.pink,
-          accentColor: Colors.amber,
-          canvasColor: Color.fromRGBO(255, 254, 229, 1),
-          fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith(
-                bodyText1: TextStyle(
-                  color: Color.fromRGBO(20, 51, 51, 1),
-                ),
-                bodyText2: TextStyle(
-                  color: Color.fromRGBO(20, 51, 51, 1),
-                ),
-                headline6: TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'RobotoCondensed',
-                  fontWeight: FontWeight.bold,
-                ),
-                button: TextStyle(
-                  fontSize: 20,
-                ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
               ),
-        ),
-        home: const CategoriesScreen(),
-        routes: {
-          CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
-          MealDetailScreen.routeName: (context) => MealDetailScreen(),
-        });
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+              button: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+      ),
+      home: const CategoriesScreen(),
+      routes: {
+        CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (context) => MealDetailScreen(),
+      },
+      onGenerateRoute: (settings) {
+        // print(settings.arguments);
+        // return MaterialPageRoute(builder: (context) => CategoriesScreen());
+      },
+      onUnknownRoute: (settings) {
+        print(settings);
+        return MaterialPageRoute(builder: (context) => CategoriesScreen());
+      },
+    );
   }
 }
