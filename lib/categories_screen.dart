551b36530f444d00cc487f15fb4254aa1557785d
TORU AOKI <Toru.Aoki@ibm.com>
2022-10-31 10:24:20 +0900

    add: 162

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -8,19 +8,20 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeal'),
-      ),
+        appBar: AppBar(
+          title: const Text('DeliMeal'),
+        ),
         body: GridView(
-      children: DUMMY_CATEGORIES
-          .map((catData) =>
-              CategoryItem(title: catData.title, color: catData.color))
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 1.5,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
-    ));
+          padding: EdgeInsets.all(25),
+          children: DUMMY_CATEGORIES
+              .map((catData) =>
+                  CategoryItem(title: catData.title, color: catData.color))
+              .toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 1.5,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+              button: TextStyle(
+                fontSize: 20,
+              ),
+            ),
       ),
       home: const CategoriesScreen(),
     );
