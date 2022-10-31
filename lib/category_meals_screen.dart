97dbde626e9d51ff6cc64a0851493287afe478d0
TORU AOKI <Toru.Aoki@ibm.com>
2022-10-31 10:45:08 +0900

    add: 163

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  const CategoryMealsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('The Recipes'),
+        ),
+        body: Center(
+          child: Text('hogieho'),
+        ));
+  }
+}
