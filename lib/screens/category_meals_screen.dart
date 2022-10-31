dc4543e6248b824f7cad0be0d0f95bc97e735cd4
TORU AOKI <Toru.Aoki@ibm.com>
2022-10-31 15:33:43 +0900

    add 169

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dummy_data.dart';
+import 'package:flutter_youtube1/widgets/meal_item.dart';
+import '../dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -23,7 +24,12 @@ class CategoryMealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(
           itemBuilder: (context, index) {
-            return Text(categoryMeals[index].title);
+            return MealItem(
+                title: categoryMeals[index].title,
+                imageUrl: categoryMeals[index].imageUrl,
+                duration: categoryMeals[index].duration,
+                complexity: categoryMeals[index].complexity,
+                affordability: categoryMeals[index].affordability);
           },
           itemCount: categoryMeals.length),
     );
