5ed70f30c46c805c2a4bff057fa24b5f37585137
TORU AOKI <Toru.Aoki@ibm.com>
2022-11-01 16:37:04 +0900

    add: 191

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -17,27 +17,33 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        child: Image.network(
-          product.imageUrl,
-          fit: BoxFit.cover,
-        ),
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
-            color: Theme.of(context).accentColor,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context)
+              .pushNamed('/product-detail', arguments: {'productId': product.id});
+        },
+        child: GridTile(
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
           ),
-          title: Text(
-            product.title,
-            style: TextStyle(fontSize: 18),
-            textAlign: TextAlign.center,
-          ),
-          trailing: IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {},
-            color: Theme.of(context).accentColor,
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: IconButton(
+              icon: Icon(Icons.favorite),
+              onPressed: () {},
+              color: Theme.of(context).accentColor,
+            ),
+            title: Text(
+              product.title,
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
+              color: Theme.of(context).accentColor,
+            ),
           ),
         ),
       ),
