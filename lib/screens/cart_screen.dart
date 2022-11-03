e80a910bbfd1a6edb363af4bf1eba8395b82d204
TORU AOKI <Toru.Aoki@ibm.com>
2022-11-03 13:55:14 +0900

    add: 207

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -54,6 +54,7 @@ class CartScreen extends StatelessWidget {
             child: ListView.builder(
               itemBuilder: (context, index) => CartItem(
                 id: cart.items.values.toList()[index].id,
+                productId: cart.items.keys.toList()[index],
                 price: cart.items.values.toList()[index].price,
                 quantity: cart.items.values.toList()[index].quantity,
                 title: cart.items.values.toList()[index].title,
