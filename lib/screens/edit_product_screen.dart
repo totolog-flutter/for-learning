4a85738b22c7c19c2d130328bcd9a5fa921562fe
TORU AOKI <Toru.Aoki@ibm.com>
2022-11-04 10:27:33 +0900

    add: 221

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatefulWidget {
+  const EditProductScreen({super.key});
+
+  @override
+  State<EditProductScreen> createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
