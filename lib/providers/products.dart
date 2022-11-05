165698515acc81e42a46830849a08b846c03deaa
TORU AOKI <Toru.Aoki@ibm.com>
2022-11-05 17:34:34 +0900

    add: 234

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -85,4 +85,9 @@ class Products with ChangeNotifier {
       notifyListeners();
     }
   }
+
+  void deleteProduct(String id) {
+    _items.removeWhere((product) => product.id == id);
+    notifyListeners();
+  }
 }
--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_youtube1/screens/edit_product_screen.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
+import '../screens/edit_product_screen.dart';
 
 class UserProductItem extends StatelessWidget {
   final String id;
@@ -27,7 +29,9 @@ class UserProductItem extends StatelessWidget {
               color: Theme.of(context).primaryColor,
             ),
             IconButton(
-              onPressed: null,
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
               icon: Icon(Icons.delete),
               color: Theme.of(context).errorColor,
             )
