b76090816ca2ea1712bf614f29cd8204200b9b48
TORU AOKI <Toru.Aoki@ibm.com>
2022-11-06 11:16:14 +0900

    add: 256

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +19,28 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    var oldFavorite = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url = Uri.parse(
+        'https://flutter-shop-app-1d512-default-rtdb.firebaseio.com/products$id.json');
+    try {
+      final response = await http.patch(
+        url,
+        body: jsonEncode({'isFavorite': isFavorite}),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldFavorite);
+      }
+    } catch (error) {
+      _setFavValue(oldFavorite);
+      notifyListeners();
+    }
   }
 }
